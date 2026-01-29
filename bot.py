b9cd85b209c359b9cab6f198f161660019bc5223
takehiro0123689-lab <takehiro.0123.689@gmail.com>
2026-01-29 10:09:20 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"ログイン完了: {bot.user}")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("こんにちは！")
+
+bot.run(os.environ["DISCORD_TOKEN"])
 
